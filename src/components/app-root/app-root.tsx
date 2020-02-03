2178298793cd38057acfea59d5f5e059ae117117
Ettiene Mare <Ettiene.Mare@derivco.co.za>
2020-02-03 13:02:30 +0200

    update urls

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -23,8 +23,8 @@ export class AppRoot {
         <main>
           <stencil-router>
             <stencil-route-switch scrollTopOffset={0}>
-              <stencil-route url='/' component='app-home' exact={true} />
-              <stencil-route url='/content/:process' component='app-content' />
+              <stencil-route url='/vega' component='app-home' exact={true} />
+              <stencil-route url='vega/content/:process' component='app-content' />
             </stencil-route-switch>
           </stencil-router>
         </main>
