a707f51c1e6fea1317cbaef1d2d5a7311216541b
Ettiene Mare <Ettiene.Mare@derivco.co.za>
2020-02-03 13:23:11 +0200

    fix links

--- src/components/app-menu/app-menu.tsx
+++ src/components/app-menu/app-menu.tsx
@@ -9,7 +9,7 @@ export class AppMenu {
   render() {
       return(
         <div>
-          <stencil-route-link url='/vega'><i class="material-icons">home</i>Home</stencil-route-link>
+          <stencil-route-link url='/'><i class="material-icons">home</i>Home</stencil-route-link>
           <stencil-route-link url='/content/login'><i class="material-icons">lock_open</i>Login</stencil-route-link>
           <stencil-route-link url='/content/register'><i class="material-icons">account_box</i>Register</stencil-route-link>
           <stencil-route-link url='/content/banking'><i class="material-icons">account_balance</i>Banking</stencil-route-link>
